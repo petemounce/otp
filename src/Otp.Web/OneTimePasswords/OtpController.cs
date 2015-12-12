52653a57a61353a9122db12f25bdaabd5059be0a
Peter Mounce <peter.mounce@just-eat.com>
2015-12-12 17:13:26 +0000

    controller stub

---
+++ src/Otp.Web/OneTimePasswords/OtpController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Otp.Web.OneTimePasswords
+{
+    public class OtpController : ApiController
+    {
+        // GET: api/Otp
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Otp/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Otp
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Otp/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Otp/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
