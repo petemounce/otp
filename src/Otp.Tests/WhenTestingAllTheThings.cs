5ad3d6bcb09661bd42ae4736fe7ded5c732d28d6
Peter Mounce <peter.mounce@just-eat.com>
2015-12-12 17:38:59 +0000

    DRY: take advantage of xunit IDisposable

--- src/Otp.Tests/WhenTestingAllTheThings.cs
+++ src/Otp.Tests/WhenTestingAllTheThings.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using Microsoft.Owin.Testing;
 using Otp.Web;
 using Shouldly;
@@ -6,26 +7,32 @@ using Xunit;
 
 namespace Otp.Tests
 {
-    public class WhenTestingAllTheThings
+    public class WhenTestingAllTheThings : IDisposable
     {
+        private readonly TestServer _server;
+
+        public WhenTestingAllTheThings()
+        {
+            _server = TestServer.Create<Startup>();
+        }
+
+        public void Dispose()
+        {
+            _server?.Dispose();
+        }
+
         [Fact]
         public async void CanGetOtpList()
         {
-            using (var server = TestServer.Create<Startup>())
-            {
-                var res = await server.HttpClient.GetAsync("/api/otp").ConfigureAwait(false);
-                res.StatusCode.ShouldBe(HttpStatusCode.OK);
-            }
+            var res = await _server.HttpClient.GetAsync("/api/otp").ConfigureAwait(false);
+            res.StatusCode.ShouldBe(HttpStatusCode.OK);
         }
 
         [Fact]
         public async void CanGetOneOtp()
         {
-            using (var server = TestServer.Create<Startup>())
-            {
-                var res = await server.HttpClient.GetAsync("/api/otp/1").ConfigureAwait(false);
-                res.StatusCode.ShouldBe(HttpStatusCode.OK);
-            }
+            var res = await _server.HttpClient.GetAsync("/api/otp/1").ConfigureAwait(false);
+            res.StatusCode.ShouldBe(HttpStatusCode.OK);
         }
     }
 }
